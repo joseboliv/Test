27433d9bd994d375d8babc033570431d543f0c29
jose_boliv <jose.bolivar@sto.ad>
2022-08-13 18:34:36 -0400

    Create project

---
+++ HackerRank/Program - Copy.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace HackerRank
+{
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int iterationNumber = 1;
+    //        int length = 32;
+    //        char pad = '0';
+    //        Console.Write("Inserte cantidad de iteraciones \n");
+    //        string inputIterationNumber = Console.ReadLine();
+    //        iterationNumber = inputIterationNumber.ConvertStringToInteger();
+            
+    //        string[] finishResult = new string[iterationNumber];
+
+    //        for (int i = 0; i < iterationNumber; i++)
+    //        {
+    //            string binary = string.Empty;
+    //            int remainder;
+    //            Console.Write("Inserte Valor Bit:\n");
+    //            string aux = Console.ReadLine();
+    //            int inputInteger = aux.ConvertStringToInteger();
+
+    //            while (inputInteger > 0)
+    //            {
+    //                remainder = inputInteger % 2;
+    //                inputInteger /= 2;
+    //                binary = remainder.ToString() + binary;
+    //            }
+
+    //            if (binary.Length < length)
+    //            {
+    //                binary = binary.PadLeft(length, pad);
+    //            }
+
+    //            string[] complete = new string[binary.Length];
+
+    //            for (int j = 0; j < binary.Length; j++)
+    //            {
+    //                if (binary[j].ToString() == "1")
+    //                {
+    //                    complete[j] = "0";
+    //                }
+    //                else
+    //                {
+    //                    complete[j] = "1";
+    //                }
+    //            }
+    //            binary = string.Join("", complete);
+    //            finishResult[i] = binary;
+    //        }
+    //        finishResult.PrintResult();
+    //    }
+    //}
+
+    //public static class Extensions
+    //{
+    //    public static int ConvertStringToInteger(this string value)
+    //    {
+    //        int aux = 0;
+    //        int.TryParse(value, out aux);
+    //        return aux;
+    //    }
+
+    //    public static void PrintResult(this string[] value)
+    //    {
+    //        foreach (var item in value)
+    //        {
+    //            Console.WriteLine(Convert.ToUInt64(item, 2));
+    //        }
+    //    }
+
+    //}
+}
---
+++ HackerRank/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace HackerRank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLength = 5;
+            int inputnumbers = 0;
+            Console.Write("Inserte longitud de caracteres \n");
+            string inputIterationNumber = Console.ReadLine();
+            arrayLength = inputIterationNumber.ConvertStringToInteger();
+
+            Console.Write("Inserte Valor Bit:\n");
+            string inputValuesNumber = Console.ReadLine().Replace(" ", string.Empty);
+            //string inputValuesNumber = "1 2 1 2 4".Replace(" ", string.Empty);
+            inputnumbers = Convert.ToInt32(inputValuesNumber);
+
+            int cant = 0;
+            int number = 0;
+            if (arrayLength > 1)
+            {
+                for (int i = 0; i < inputValuesNumber.Length; i++)
+                {
+                    int aux = int.Parse(inputValuesNumber[i].ToString());
+                    int auxCount = 0;
+
+                    for (int j = 0; j < inputValuesNumber.Length; j++)
+                    {
+                        int g = int.Parse(inputValuesNumber[j].ToString());
+                        if (aux == int.Parse(inputValuesNumber[j].ToString()))
+                        {
+                            auxCount++;
+                        }
+                    }
+
+                    if (i == 0)
+                    {
+                        number = aux;
+                        cant = auxCount;
+                    }
+                    else if (cant > auxCount)
+                    {
+                        cant = auxCount;
+                        number = aux;
+                    }
+                }
+            }
+            inputnumbers = number;
+            Console.WriteLine($"{inputnumbers}");
+            Console.ReadLine();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static int ConvertStringToInteger(this string value)
+        {
+            int aux = 0;
+            int.TryParse(value, out aux);
+            return aux;
+        }
+
+        public static void PrintResult(this string[] value)
+        {
+            foreach (var item in value)
+            {
+                Console.WriteLine(Convert.ToUInt64(item, 2));
+            }
+        }
+
+    }
+}
